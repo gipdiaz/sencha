43cd30b5825db302281b6f538a892eefd3d347cb
Gastón Díaz <gipdiaz@gmail.com>
2012-08-27 16:29:32 -0300

    se agrego el blog

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ Ext.application({
 			fullscreen: true,
 			tabBarPosition: 'bottom',
 			items:[
+				// Pestaña Home
 				{
 					title: 'Home',
 					iconCls: 'home',
@@ -16,7 +17,48 @@ Ext.application({
                         "to use tabs, lists and forms to create a simple app</p>",
                         '<h2>Sencha Touch (2.0.0)</h2>'
                     ].join("")
-				}
+				},
+				// Pestaña Blog
+				{
+                    xtype: 'nestedlist',
+                    title: 'Blog',
+                    iconCls: 'star',
+                    displayField: 'title',
+
+                    store: {
+                        type: 'tree',
+
+                        fields: [
+                            'title', 'link', 'author', 'contentSnippet', 'content',
+                            {name: 'leaf', defaultValue: true}
+                        ],
+
+                        root: {
+                            leaf: false
+                        },
+
+                        proxy: {
+                            type: 'jsonp',
+                            url: 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://feeds.feedburner.com/SenchaBlog',
+                            reader: {
+                                type: 'json',
+                                rootProperty: 'responseData.feed.entries'
+                            }
+                        }
+                    },
+
+                    detailCard: {
+                        xtype: 'panel',
+                        scrollable: true,
+                        styleHtmlContent: true
+                    },
+
+                    listeners: {
+                        itemtap: function(nestedList, list, index, element, post) {
+                            this.getDetailCard().setHtml(post.get('content'));
+                        }
+                    }
+                }
 			]		
 		});
 	}
